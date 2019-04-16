90b7de4fcde2ea6409b2865e079e750d6e28b30e
jmx <695205458@qq.com>
2019-04-16 10:36:36 +0800

    创建booktest模块

---
+++ demo01/booktest/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class BooktestConfig(AppConfig):
+    name = 'booktest'
+
+'''
+应用配置文件
+'''
---
+++ demo01/booktest/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+'''
+Django MVT  M
+ORM 对象中O 
+需要定义实体类
+'''
