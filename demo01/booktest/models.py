b9f97a9ddf5c143a9d638ac88128453d5dead601
jmx <695205458@qq.com>
2019-04-16 14:53:22 +0800

    生成迁移文件  生成数据库 导入内容

--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class BookInfo(models.Model):
+    btitle=models.CharField(max_length=20)
+    bpub_date=models.DateTimeField()
+
+class HeroInfo(models.Model):
+    hname=models.CharField(max_length=20)
+    hgender=models.BooleanField()
+    #外键 第一个参数为表名 第二个参数代表删除类型
+    hBook=models.ForeignKey('BookInfo',on_delete=models.CASCADE)
 
 
 '''
