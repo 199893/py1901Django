10471e5d88dd8cc77a79f81c45a97e0b3f2a9aa5
jmx <695205458@qq.com>
2019-04-17 11:21:56 +0800

    配置自定义管理界面，关联注册

--- demo01/booktest/admin.py
+++ demo01/booktest/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
-
+from .models import BookInfo,HeroInfo
 # Register your models here.
+
+class HeroInfoInline(admin.StackedInline):
+    '''关联'''
+    model = HeroInfo
+    #关联数
+    extra = 1
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    #排序显示
+    list_display = ['hname','hgender','hBook']
+    #右侧显示过滤框
+    list_filter = ['hname']
+    #查询框
+    search_fields = ['hname']
+    #分页
+    list_per_page = 1
+
+class BookInfoAdmin(admin.ModelAdmin):
+    # 排序显示
+    list_display = ['btitle', 'bpub_date']
+    # 右侧显示过滤框
+    list_filter = ['btitle']
+    # 查询框
+    search_fields = ['btitle']
+    # 分页
+    list_per_page = 1
+
+    #关联，添加书时可添加英雄信息
+    inlines = [HeroInfoInline]
+
+
+
+admin.site.register(BookInfo,BookInfoAdmin)
+admin.site.register(HeroInfo,HeroInfoAdmin)
+
 
 '''
 通过少量代码实现强大的后台管理
--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -6,12 +6,17 @@ class BookInfo(models.Model):
     btitle=models.CharField(max_length=20)
     bpub_date=models.DateTimeField()
 
+    def __str__(self):
+        return self.btitle
+
 class HeroInfo(models.Model):
     hname=models.CharField(max_length=20)
     hgender=models.BooleanField()
     #外键 第一个参数为表名 第二个参数代表删除类型
     hBook=models.ForeignKey('BookInfo',on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.hname
 
 '''
 Django MVT  M
